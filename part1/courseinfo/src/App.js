0e98babb6b772232507a99895858a098f678e35b
liaisontw <liaison.tw@gmail.com>
2022-07-16 18:46:30 +0800

    exercise 1.1 begin

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,10 +1,46 @@
-
-function App() {
+/*
+const Content = ... {
   return (
     <div>
-      <p>Hello world</p>
+      <Part .../>
+      <Part .../>
+      <Part .../>
     </div>
-  );
+  )
 }
+*/
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  //<Header course={course} />
+  //<Content ... />
+  //<Total ... />
+
+
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
 
 export default App;
